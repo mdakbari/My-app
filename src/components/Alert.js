b33d9ec4354b029969418217775c95ed6a0e08c9
mdakbari <103118231+mdakbari@users.noreply.github.com>
2022-04-27 12:46:28 +0530

    Add files via upload

---
+++ src/components/Alert.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Alert(props) {
+  const captital = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{captital(props.alert.type)}</strong>: {props.alert.msg}
+      </div>
+    )
+  );
+}
+export default Alert;
